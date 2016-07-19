1e46a7d9f6cdd96d620bc8024ab50288f2a41bd6
Stanislav Lieskovský <stano.lieskovsky@gmail.com>
2016-07-19 21:57:47 +0200

    class of excercise3 added

---
+++ Cvicenie3/Cvicenie3/class.cs
@@ -0,0 +1,33 @@
+﻿namespace Cvicenie3
+{
+    /// <summary>
+    /// Uzol obojsmerne zretazeneho zoznamu.
+    /// </summary>
+    /// <typeparam name="T">Typ prvku, ktory bude uzol obsahovat v <see cref="Data"/>.</typeparam>
+    class DoublyLinkedNode<T>
+    {
+        /// <summary>
+        /// Odkaz na predchadzajuci uzol.
+        /// </summary>
+        public DoublyLinkedNode<T> Previous { get; set; }
+        
+        /// <summary>
+        /// Odkaz na nasledujuci uzol.
+        /// </summary>
+        public DoublyLinkedNode<T> Next { get; set; }
+        
+        /// <summary>
+        /// Prvok typu T.
+        /// </summary>
+        public T Data { get; set; }
+
+        /// <summary>
+        /// Vytvori uzol pre obojsmerne zretazeny zoznam.
+        /// </summary>
+        /// <param name="data">Prvok, ktory bude uzol obsahovat.</param>
+        public DoublyLinkedNode(T data)
+        {
+            Data = data;
+        }
+    }
+}
