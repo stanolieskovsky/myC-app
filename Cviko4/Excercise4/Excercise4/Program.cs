7de286526700a8e40f5ef2d519466e6dbd92b7b6
Stanislav Lieskovský <stano.lieskovsky@gmail.com>
2016-07-22 23:26:11 +0200

    new console aplication created

---
+++ Cviko4/Excercise4/Excercise4/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Excercise4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
