aed8716e819419ec681a019781ecc034987b0355
Stanislav Lieskovský <stano.lieskovsky@gmail.com>
2017-11-17 12:44:15 +0100

    recommit for all sln , repair my visual

---
+++ ConsoleTestFunctionality/ConsoleTestFunctionality/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ConsoleTestFunctionality
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("ahoj svet");
+            Console.ReadLine();
+        }   
+    }
+}
---
+++ Cviko4/ConsolTest17.11/ConsolTest17.11/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ConsolTest17._11
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
